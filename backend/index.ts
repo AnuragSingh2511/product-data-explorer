e7b5ea4dc73238f7bfba3c69a6528c8e860ee488
Anurag Singh <anurocks.aps@gmail.com>
2025-09-18 13:43:05 +0530

    test

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import type { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
+
 dotenv.config();
 
 const app: Express = express();
